e21ac5584c0195ef1b40c4a5d6808469369bc4ef
rsplanes <rsplanes@gmail.com>
2018-04-01 11:39:23 +0200

    Complete tutorial about JWT Auth

---
+++ SamplesNetCoreWebApi/WebApiJwt/Entities/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiJwt.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            const string databaseServer = "";
+            const string databaseName = "";
+            const string databaseUser = "";
+            const string databasePass = "";
+
+            return $"Server={databaseServer};" +
+                   $"database={databaseName};" +
+                   $"uid={databaseUser};" +
+                   $"pwd={databasePass};" +
+                   $"pooling=true;";
+        }
+    }
+}
